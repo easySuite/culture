6a2284c09a17594be2db6300338460df8db2abda
Mihaela Stavila <ms@inleadmedia.com>
2018-05-30 18:19:42 +0300

    hotfix - Fix collapse for event filter.

--- script/culture.js
+++ script/culture.js
@@ -5,7 +5,16 @@
     attach: function (context, settings) {
       // Display all filters for big screens.
       if ($(window).width() > 768) {
-        $('a.fieldset-title', context).click();
+        //$('a.fieldset-title', context).click();
+
+        $( "fieldset.bef-select-as-checkboxes-fieldset" ).removeClass( "collapsible" );
+        $( "fieldset.bef-select-as-checkboxes-fieldset" ).removeClass( "collapsed" );
+        $( "fieldset.bef-select-as-checkboxes-fieldset" ).removeClass( "collapse-processed" );
+        $('.fieldset-legend > a').replaceWith(function() {
+          return $('<div/>', {
+              html: this.innerHTML
+          });
+        });
       }
     }
   };
