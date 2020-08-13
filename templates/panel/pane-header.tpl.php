1d8e5702461f39d9532d3f7bdd720fe573f4218b
Martin Cording <mc@inlead.dk>
2020-08-13 23:30:36 +0200

    Hotfix: Print sitename instead of hardcode.

--- templates/panel/pane-header.tpl.php
+++ templates/panel/pane-header.tpl.php
@@ -18,12 +18,12 @@
  */
 ?>
 <div class="logo">
-  <a href="<?php print $front_page; ?>" title="<?php print t('Oplev Brøndby'); ?>" rel="home">
+  <a href="<?php print $front_page; ?>" rel="home">
     <div class="site-name-container<?php if ($logo): ?> logo-container<?php
    endif; ?>">
       <div class="site-name"><?php print $site_name; ?></div>
       <?php if ($logo): ?>
-        <img src="<?php print $logo; ?>" />
+        <img alt="<?php print $site_name; ?>" title="<?php print $site_name; ?>" src="<?php print $logo; ?>" />
         <span class="vertical-helper"></span>
       <?php endif; ?>
     </div>
